bc6bcfb10ee97a0689a55c0b28e01163c1faf410
Priyanka <priyanka.chowdhury2020@vitstudent.ac.in>
2021-09-03 20:03:56 +0530

    ml model added+layouts modified

--- android/app/src/main/java/org/tensorflow/lite/examples/posenet/MainActivity.kt
+++ android/app/src/main/java/org/tensorflow/lite/examples/posenet/MainActivity.kt
@@ -3,13 +3,31 @@ package org.tensorflow.lite.examples.posenet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.bumptech.glide.Glide
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView: ImageView = findViewById(R.id.gif)
+
+        Glide.with(this).load(R.drawable.giphy).into(imageView)
+
     }
+
+
 
     fun passToFitness(view: View) {startActivity(Intent(this, FitnessActivity::class.java))}
     fun passToWorkout(view: View) {startActivity(Intent(this, WorkoutActivity::class.java))}
